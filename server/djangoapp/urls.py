e10187323e218519da0e380ec2c62ffd0a602070
FFC <69189869+FFC20@users.noreply.github.com>
2024-11-19 18:44:46 +0300

    Update urls.py
    Linter suggested updates 2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,11 +16,23 @@ urlpatterns = [
     path('get_cars', views.get_cars, name='getcars'),
     # path for get_dealerships view
     path('get_dealers/', views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path(
+        'get_dealers/<str:state>',
+        views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
     # path for dealer_details view
-    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
+    path(
+        'dealer/<int:dealer_id>',
+        views.get_dealer_details,
+        name='dealer_details'
+    ),
     # path for dealer reviews view
-    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
+    path(
+        'reviews/dealer/<int:dealer_id>',
+        views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
     # path for add a review view
     path('add_review', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
